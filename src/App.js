e3fab9971eb6203ff0373ff8aaa38fdbcd931165
sunjing <sunjing@mzhcorp.com>
2022-03-28 10:09:14 +0800

    redux practice count and log

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, {useState} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {increment, decrement, signin} from './redux/actions';
+
 const App = ({}) => {
   const [count, setCount] = useState(0);
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.logged);
+  const dispatch = useDispatch();
+
   const changeCount = (msg) => {
     setCount(msg);
   }
   return (
     <div>
       <h1>{count}</h1>
+      <h1>Counter {JSON.stringify(counter)}</h1>
+      {isLogged && <h3>Valuable Information I shouldn't see</h3>}
+      <button onClick={() => dispatch(increment())}>+</button> 
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(signin())}>LOG_IN</button>
       <Counter value={count} changeCount={changeCount}/>
       <Number value={count} changeCount={changeCount}/>
     </div>
